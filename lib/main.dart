47e12c6c56e21c26d8c117255d5bc51ade303143
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-03-05 19:36:20 +0530

    Router Core

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'package:shoot/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage());
+    return MaterialApp.router(
+      routeInformationParser: MyRouter().router.routeInformationParser,
+      routerDelegate: MyRouter().router.routerDelegate,
+    );
   }
 }
---
+++ lib/router.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:shoot/home.dart';
+
+class MyRouter {
+  GoRouter router = GoRouter(routes: [
+    GoRoute(
+      name: 'home',
+      path: '/',
+      pageBuilder: (context, state) {
+        return const MaterialPage(child: HomePage());
+      },
+    )
+  ]);
+}
