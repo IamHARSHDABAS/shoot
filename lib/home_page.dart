9aac649a92a028f3c72b90fc03f6211ed666de18
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-03-05 18:45:02 +0530

    Better Home UI

---
+++ lib/home_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(128, 0, 255, 1),
+        title: const Center(
+          child: Text(
+            'Shoot',
+            style: TextStyle(
+                color: Color.fromRGBO(242, 229, 255, 1), fontSize: 24),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 64,
+            ),
+            Container(
+              width: 348,
+              height: 128,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: const Color.fromRGBO(128, 0, 255, 1)),
+              child: const Center(
+                child: Text(
+                  'Match',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Color.fromRGBO(242, 229, 255, 1), fontSize: 32),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 64,
+            ),
+            Container(
+              width: 348,
+              height: 128,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: const Color.fromRGBO(128, 0, 255, 1)),
+              child: const Center(
+                child: Text(
+                  'Advanced',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Color.fromRGBO(242, 229, 255, 1), fontSize: 32),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'Screen/home_page.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomePage());
+    return const MaterialApp(
+      home: HomePage());
   }
 }
